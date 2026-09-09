2dd4b77175a1c31e71fe66667e68a2e90c727463
unknown <vermaaaharsh@gmail.com>
2026-09-09 23:29:37 +0530

    initial upload

---
+++ Addition.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ int main() {
+    int x,y,sum;
+    printf("Enter any number :");
+    scanf("%d", &x);
+    printf("Enter second number :");
+    scanf("%d", &y);
+
+    sum = x + y;
+    printf("Sum of two numbers is : %d", sum);
+        return 0;
+ }
